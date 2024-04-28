fe1fc9ce5c1fd85af65362d91c2c0dcf8a82dc72
unknown <computersciencejinja@gmail.com>
2024-04-28 14:07:48 +0300

    pushing to render

--- dpmas/settings.py
+++ dpmas/settings.py
@@ -81,15 +81,20 @@ WSGI_APPLICATION = 'dpmas.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-DATABASES = {
-    'default': dj_database_url.config(
-        # Replace this value with your local database's connection string.
-        default='postgresql://postgres:postgres@localhost:5432/mysite',
-        conn_max_age=600
-    )
-}
 
+if not DEBUG:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    }
 
+else:
+    DATABASES = {
+        'default': dj_database_url.config(
+            # Replace this value with your local database's connection string.
+            default='postgresql://postgres:postgres@localhost:5432/mysite',
+            conn_max_age=600
+        )
+    }
 
 
 
